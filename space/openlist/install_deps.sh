d8b03f75a5401d1e13aed0faea56b9f3ddfed51b
Jyf0214 <169313142+Jyf0214@users.noreply.github.com>
2025-10-01 17:28:46 +0800

    更新 install_deps.sh

--- space/openlist/install_deps.sh
+++ space/openlist/install_deps.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 set -e
 
-echo "--- [BUILD SCRIPT - DEPS] Installing base dependencies ---"
+echo "--- [BUILD SCRIPT - DEPS] Installing dependencies ---"
 
 apt-get update && apt-get install -y --no-install-recommends \
     curl \
-    wget \
     tar \
     python3 \
     python3-pip \
     supervisor \
-    ca-certificates \
     git \
     rclone \
+    gettext-base \
 && rm -rf /var/lib/apt/lists/*
-
-echo "--- [BUILD SCRIPT - DEPS] Installing Litestream ---"
-# 从 GitHub 下载并安装 Litestream 的 .deb 包，这是最干净的方式
-LITESTREAM_VERSION="0.3.13" # 你可以根据需要更新版本
+# ... (litestream 和重命名部分保持不变) ...
+LITESTREAM_VERSION="0.3.13"
 wget -q -O /tmp/litestream.deb "https://github.com/benbjohnson/litestream/releases/download/v${LITESTREAM_VERSION}/litestream-v${LITESTREAM_VERSION}-linux-amd64.deb"
 apt-get install -y /tmp/litestream.deb
 rm /tmp/litestream.deb
-
-echo "--- [BUILD SCRIPT - DEPS] Camouflaging binaries ---"
 mv /usr/bin/supervisord /usr/bin/service_manager
 mv /usr/bin/supervisorctl /usr/bin/sm_ctl
 
